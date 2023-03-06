b0eec9a75d5ac058da1064f9f5d575ad9ce71dfe
prhyme <palln6935@gmail.com>
2023-03-06 18:56:06 +0530

    Add method to instance

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// creating vue instance
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: "PRHYME"
+		name: "PRHYME",
+		job: "Ninja"
+	},
+	methods: {
+		greet: function () {
+			return `Good Morning ${this.name}`;
+		}
 	}
 });
