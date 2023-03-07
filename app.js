e65b9fd6e878f2546a3b17819a77c1a683545de1
prhyme <palln6935@gmail.com>
2023-03-07 21:05:07 +0530

    Add events

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ new Vue({
 	data: {
 		name: "PRHYME",
 		job: "Ninja",
+		age: 22,
 		website: "https://nishant-pall.github.io",
-		websiteTag: '<a href="https://nishant-pall.github.io">website</a>'
+		websiteTag: '<a href="https://nishant-pall.github.io">website</a>',
+		x: 0,
+		y: 0
 	},
 	methods: {
 		greet: function () {
 			return `Good Morning ${this.name}`;
+		},
+		add: function (inc) {
+			this.age += inc;
+		},
+		subtract: function (dec) {
+			this.age -= dec;
+		},
+		calcPos: function (event) {
+			this.x = event.offsetX;
+			this.y = event.offsetY;
 		}
 	}
 });
