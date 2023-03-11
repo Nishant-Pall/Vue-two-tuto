afdc7c461a0e644a3b4785596f026380e2dde88e
prhyme <palln6935@gmail.com>
2023-03-11 12:20:11 +0530

    Clean vue file

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title:"Vue file"
+    }
+  },
+  methods:{
+    greeting:function(){
+      return 'HELOOO'
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 </style>
