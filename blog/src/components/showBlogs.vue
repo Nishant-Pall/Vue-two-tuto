e02108c6a7fbaa510a785f2f48fdbb243e90dfb3
prhyme <palln6935@gmail.com>
2023-03-13 15:03:14 +0530

    Create display blogs component

---
+++ blog/src/components/showBlogs.vue
@@ -0,0 +1,38 @@
+<template >
+	<div id="show-blogs">
+		<h1>All Blogs</h1>
+		<div class="single-blog"></div>
+	</div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			blogs: [],
+		};
+	},
+	methods: {},
+	created() {
+		axios
+			.get("#")
+			.then((data) => {
+				this.blogs = data.body.slice(0, 10);
+			})
+			.catch((err) => console.log(err));
+	},
+};
+</script>
+<style scoped>
+#show-blogs {
+	max-width: 800px;
+	margin: 0 auto;
+}
+.single-blog {
+	padding: 20px;
+	margin: 20px 0;
+	box-sizing: border-box;
+	background: #eee;
+}
+</style>
